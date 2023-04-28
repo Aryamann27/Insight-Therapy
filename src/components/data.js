6ba1db8467454f2ef500e95570f25426b56a6cc5
Aryamann27 <109002742+Aryamann27@users.noreply.github.com>
2023-04-28 21:02:43 +0530

    Create data.js

---
+++ src/components/data.js
@@ -0,0 +1,32 @@
+const rev = [
+    {
+        id:"1",
+        name:"Susan Smith",
+        type:"Depression",
+        text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+        id:"2",
+        name:"Anna Johnson",
+        type:"Insomnia",
+        text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+        id:"3",
+        name:"Peter Jones",
+        type:"Anxiety, Stress",
+        text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+        id:"4",
+        name:"Bill Anderson",
+        type:"Depression",
+        text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+        id:"5",
+        name:"Allen Andrew",
+        type:"Substance Abuse",
+        text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    }
+]
